9acbc40924d9bea0df2200b96d80e2270a769294
Antoine Marteau <43928194+Antoinemarteau@users.noreply.github.com>
2025-11-15 12:11:31 +1100

    Julia version 1.12.1 fix hash

--- julia.nix
+++ julia.nix
@@ -2,7 +2,7 @@
 
 let
   versionShas = {
-    "1.12.1" = "sha256-cj6HjGQiIMwCUaDhN1jAWaOJytx/ATdv6vHqc4j+j5w=";
+    "1.12.1" = "sha256-fSrdnudO4vErXCaLwZR5TMUupED4aH+6sp22r++/abc=";
     "1.11.5" = "sha256-cj6HjGQiIMwCUaDhN1jAWaOJytx/ATdv6vHqc4j+j5w=";
     "1.11.1" = "sha256-zKjRPcRQfk9ioSkyIpMxPuV08wDU3559swt7QcX4qPM=";
     "1.11.0" = "sha256-vPgVVT/aLteRBSTIyqGJyOgZGkCnmd2LX77Q2d1riCw=";
