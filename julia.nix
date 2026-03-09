c9863109328dc0b883746beacb6f517f0b681666
Antoine Marteau <antoine.marteau@protonmail.com>
2026-03-09 17:22:56 +1100

    replace 1.12.4 with 1.12.5

--- julia.nix
+++ julia.nix
@@ -2,7 +2,7 @@
 
 let
   versionShas = {
-    "1.12.4" = "sha256-fSrdnudO4vErXCaLwZR5TMUupED4aH+6sp22r++/abc=";
+    "1.12.5" = "sha256-fSrdnudO4vErXCaLwZR5TMUupED4aH+6sp22r++/abc=";
     "1.12.2" = "sha256-fSrdnudO4vErXCaLwZR5TMUupED4aH+6sp22r++/abc=";
     "1.12.1" = "sha256-fSrdnudO4vErXCaLwZR5TMUupED4aH+6sp22r++/abc=";
     "1.11.5" = "sha256-cj6HjGQiIMwCUaDhN1jAWaOJytx/ATdv6vHqc4j+j5w=";
